20a8e21a1c32c833530909a5abedbeb64e9dfd77
shaunalon <shaunalon9@gmail.com>
2025-11-12 11:11:44 -0500

    Fixes again.

--- calculator.py
+++ calculator.py
@@ -9,7 +9,7 @@ def square_root(a):
         return math.sqrt(a)
     except ValueError:
         print("Cannot take the square root of a negative number.")
-
+        raise
 def hypotenuse(a,b):
     return math.hypot(a,b)
 
