f43165a716963379861aec2416e578b29d2b6ef5
thaveeshgallage-hub <thaveeshgallage@gmail.com>
2025-11-12 10:27:32 -0500

    hi

--- calculator.py
+++ calculator.py
@@ -4,8 +4,27 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
+import math
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b==0:
+        raise ZeroDivisionError("Cannot divide by zero.")
+    return a / b
+
+def log(a, b):
+    if a<=0 or b<=0:
+        raise ValueError("Logarithm undefined for non-positive values.")
+    return math.log(b,a)# use math library + raise ValueError
+
+def exp(a, b):
+    return a**b
 
 
